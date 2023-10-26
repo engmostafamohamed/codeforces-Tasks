72bfe72c0dff7f24a9b7126f2d5f515e606aab3b
mostafaSystemha <m.mohamed@systemha.com>
2023-10-26 12:40:45 +0300

    create simple calculator task

--- A. Hello world by visual studio code/helloworld.cpp
+++ A. Hello world by visual studio code/helloworld.cpp
@@ -1,3 +1,20 @@
+// Given a name S. Print "Hello, (name)" without parentheses.
+
+// Input
+// Only one line containing a string S.
+
+// Output
+// Print "Hello, " without quotes, then print name.
+
+// Example
+
+// input
+// programmer
+
+// output
+// Hello, programmer
+
+
 #include <iostream>
 using namespace std;
 int main (){
--- B. Basic Data Types/Basic Data Type.cpp
+++ B. Basic Data Types/Basic Data Type.cpp
@@ -1,3 +1,36 @@
+// The following lines show some C++ data types, their format specifiers and their most common bit widths:
+
+// int : 32 Bit integer.
+// long long : 64 bit integer
+// Char : 8 bit Characters & symbols
+// Float : 32 bit real value
+// Double : 64 bit real value
+
+// For example, to print a character followed by a double:
+// char ch = 'd';
+// double d = 234.432;
+// cout << ch << " "<< d;
+ 
+// Input
+// Only one line containing the following space-separated values: int, long long, char, float and double respectively.
+
+// Output
+// Print each element on a new line in the same order it was received as input.
+
+// Don't print any extra spaces.
+
+// Example
+
+// input
+// 3 12345678912345 a 334.23 14049.30493
+
+// output
+// 3
+// 12345678912345
+// a
+// 334.23
+// 14049.3
+
 #include <iostream>
 using namespace std;
 int main (){
