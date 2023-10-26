bb433d9c9df14c77d287d49490eed26e36b9ae50
mostafaSystemha <m.mohamed@systemha.com>
2023-10-26 12:01:07 +0300

    first commit

---
+++ B. Basic Data Types/Basic Data Type.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int  integerNum;
+    long long  bigNumber;
+    char character;
+    float floatNumber;
+    double doubleNumber;
+    cin>> integerNum >> bigNumber >> character >> floatNumber >> doubleNumber;
+   cout << integerNum << endl;
+   cout << bigNumber << endl;
+   cout << character << endl;
+   cout << floatNumber << endl;
+   cout << doubleNumber << endl;
+   return 0; 
+}
